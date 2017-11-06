690e854c4ef315586f99ea4e944fb5041993289f
kelvinikome <kvnikome@gmail.com>
2017-11-06 08:26:58 +0100

    Java Exercises

--- Chapter 6/Sample.java
+++
@@ -1,8 +0,0 @@
-import java.security.SecureRandom;
-class Sample{
-	public static void main (String [] args){
-		SecureRandom rand = new SecureRandom();
-		
-		System.out.println(rand.nextInt(3));
-	}
-}
