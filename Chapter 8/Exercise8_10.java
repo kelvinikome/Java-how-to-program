9d272e8623d0940ab92256af8088c606cd45c2e6
kelvinikome <kvnikome@gmail.com>
2017-11-09 17:09:21 +0100

    Java Exercises

--- Chapter 8/Exercise8_10.java
+++ Chapter 8/Exercise8_10.java
@@ -1,6 +1,19 @@
+
 class Exercise8_10{
-    enum TrafficLight { RED, GREEN, YELLOW };
-    
+    enum TrafficLight { 
+        RED(0), GREEN(0), YELLOW(0);
+
+        public double duration;
+
+        TrafficLight(double dur){
+            this.duration = dur;
+        }
+
+        double getDuration(){
+            return duration;
+        }
+    };
+
     public static void main(String[] args) {
         
 
