1d811a47c0609f538061a30b26c1448adaa72b51
kelvinikome <kvnikome@gmail.com>
2017-11-09 12:07:51 +0100

    Java Exercises

---
+++ Chapter 7/Exercise7_11.java
@@ -0,0 +1,10 @@
+
+class Exercise7_11{
+	public static void main (String [] args){
+		int [] counts = new int[10];
+		int pos = 0;
+		for (int x : counts)
+			counts[pos++] = 9;
+		System.out.println("hey");
+	}
+}
---
+++ Chapter 7/Exercise7_8.java
@@ -0,0 +1,9 @@
+class Exercise7_8{
+	public static void main (String [] args){
+		int f[] = {1,9,3,5,7,3};
+		
+		for (int i : f)
+			System.out.print(i+",");
+		
+	}
+}
